6935b05edcc637a0c332dc5e4f91baf4cecb9ad3
Budgie007 <151498807+Budgie007@users.noreply.github.com>
2023-11-20 17:01:15 +1000

    Add files via upload

---
+++ BMI Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+                try
+                {
+                    Console.WriteLine("What is your height in metre: ");
+                    double user_height = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("What is your weight in kliograms: ");
+                    double user_weight = Convert.ToDouble(Console.ReadLine());
+                    double height_square = user_height * 2;
+                    double user_bmi = user_weight / height_square;
+                    Console.WriteLine("Your BMI is: " + String.Format("{0:0.00}", user_bmi));
+                    continue;
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Please enter detail correctly");
+                    continue;
+                }
+            }
+        }
+    }
+}
+
+
+
+            
+
+
+
+        
+
